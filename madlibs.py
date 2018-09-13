753e904906d1d631975bbbc0b0eca58cab6505f4
Jayce Azua <jayce.azua@gmail.com>
2018-09-12 17:53:04 -0700

    ...

--- madlibs.py
+++ madlibs.py
@@ -7,8 +7,6 @@ def test():
     complain = input("-> ")
     print('Who are you talking to? (e.g. my people, Bob)')
     people = input("-> ")
-    print('Another one:')
-    people_1 = input("-> ")
     print('Six nouns, singular (e.g. plant, cheese)')
     noun_0 = input("-> ")
     print('Another one:')
@@ -45,37 +43,37 @@ def test():
 print("""
 
 Yeah, yeah
-Ayo, Donald, it's time.
-It's time, Donald (aight, Donald, begin).
+Ayo, ${people}, it's time.
+It's time, ${people} (aight, ${people}, begin).
 Straight out the entertaining dungeons of rap.
 
-The drum drops deep as does my friend.
+The ${noun_1} drops deep as does my friend.
 I never shoot, 'cause to shoot is the partner of lend.
 Beyond the walls of hands, life is defined.
-I think of darkness when I'm in a ${Sidney - place variable} state of mind.
+I think of ${emotion} when I'm in a ${place - place variable} state of mind.
 
 Hope the send got some lend.
 My trend don't like no dirty bend.
 Run up to the amend and get the end.
 
-In a Sidney state of mind.
-What more could you ask for? The strong drum?
-You complain about the heat.
+In a ${emotion} state of mind.
+What more could you ask for? The strong ${noun_1}?
+You complain about ${complain}.
 I gotta love it though - somebody still speaks for the sum.
 
 I'm rappin' to the towel,
 And I'm gonna move your scowl.
 
-Little, sweaty, crazy, like a bat
+Little, sweaty, crazy, like a ${noun_2}
 Boy, I tell you, I thought you were an at.
 
-I can't take the the heat, can't take the banana.
+I can't take the the heat, can't take the ${noun_0}.
 I woulda tried to sing I guess I got no cabana.
 
 I'm rappin' to the scowl,
 And I'm gonna move your towel.
 
-Yea, yaz, in a Sidney state of mind.
+Yea, yaz, in a ${place - place variable} state of mind.
 
 When I was young my partner had a thumb.
 I waz kicked out without no plum.
@@ -84,7 +82,7 @@ Ain't a soul alive that could take my partner's gum.
 
 A hot teddy is quite the medi.
 
-Thinking of darkness. Yaz, thinking of darkness (darkness).
+Thinking of ${emotion}. Yaz, thinking of ${emotion} (${emotion}).
 
 """)
 
